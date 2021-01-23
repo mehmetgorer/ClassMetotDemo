6982cef4bf26fd25416da514afa92cf7c13e421d
Mehmet Görer <mehmetgorer@gmail.com>
2021-01-23 19:45:12 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        public void Create(Customer customer)
+        {
+            Console.WriteLine($"{customer.Name} Created");
+        }
+        public void ReadInfo(Customer customer)
+        {
+            Console.WriteLine($"---> \n{customer.Id}\n{customer.Name}\n{customer.Surname}\n{customer.Birthday}\n{customer.Address}\n==============================");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine($"---> \n{customer.Id}\n{customer.Name}\n{customer.Surname}\n{customer.Birthday}\n{customer.Address}\n==============================");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine($"{customer.Name} Deleted");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer = new Customer();
+
+            customer.Id = 1;
+            customer.Name = "Harry";
+            customer.Surname = "Potter";
+            customer.Address = "Hogwarts School of Witchcraft and Wizardry";
+            customer.Birthday = "31.07.1980";
+
+
+
+
+            CustomerManager cmanager = new CustomerManager();
+            cmanager.Create(customer);
+
+            Console.WriteLine("create method checked\n==============================");
+
+            cmanager.ReadInfo(customer);
+            Console.WriteLine("read method checked\n==============================");
+
+
+            customer.Id = 2;
+            customer.Name = "Ronald";
+            customer.Surname = "Weasley";
+            customer.Address = "Shell Cottage";
+            customer.Birthday = "01.05.1980";
+            cmanager.Update(customer);
+            Console.WriteLine("update method checked\n==============================");
+            cmanager.Delete(customer);
+            Console.WriteLine("delete method checked\n==============================");
+        }
+    }
+}
